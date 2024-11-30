0e1a298ef8da3a522851e03a06f6b8b40c855c79
Samuli <samuli.kujala@suloke.com>
2024-11-30 20:18:06 +0800

    Chinese fix

--- src/components/BookButton.jsx
+++ src/components/BookButton.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
 import "../App.css";
-
+const { t } = useTranslation();
 const BookButton = () => {
   return (
     <a href="https://calendly.com/samulikujala/30min" target="_blank" rel="noopener noreferrer">
-      <button className="bookbutton">BOOK</button>
+      <button className="bookbutton">{t(BOOK)}</button>
     </a>
   );
 };
